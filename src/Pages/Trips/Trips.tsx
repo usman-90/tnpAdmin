b9feaa5d3eb9de329a779f60a30a83876c08d03b
Talal <talalzafar84@gmail.com>
2024-05-03 12:23:09 +0500

    applied loaders on user and trips

--- src/Pages/Trips/Trips.tsx
+++ src/Pages/Trips/Trips.tsx
@@ -4,9 +4,12 @@ import { Pagination } from "antd";
 import axios from "axios";
 import { ImCancelCircle } from "react-icons/im";
 import { Toast } from "../../Components/SideToast";
+import Loader from "../../Components/loader";
 
 const UserPage: React.FC = () => {
   const [currentPage, setCurrPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [deleting, setDeleting] = useState(false);
 
   const handlePageChange = (page: number) => {
     setCurrPage(page);
@@ -15,13 +18,15 @@ const UserPage: React.FC = () => {
   const pageSize = 10;
   const totalItems = data?.length;
   const fetchData = async () => {
-    console.log("hitting", process);
+    setLoading(true);
     let res = await axios.get(
-      `${process.env.REACT_APP_SERVER_URL}/getTrips?page=${currentPage}`
+      `${process.env.REACT_APP_SERVER_URL}/getTrips?page=${currentPage}&limit=${pageSize}`
     );
     setData(res.data.data);
+    setLoading(false);
   };
   const DeleteTrip = async (id: number) => {
+    setDeleting(true);
     await axios
       .delete(`${process.env.REACT_APP_SERVER_URL}/getTrips?id=${id}`)
       .then((res) => {
@@ -36,94 +41,101 @@ const UserPage: React.FC = () => {
           title: err.response.data,
         });
       });
+    setDeleting(false);
 
     fetchData();
   };
   useEffect(() => {
     fetchData();
   }, []);
+
   return (
-    <div className="h-auto overflow-y-auto bg-white w-[95x%] relative m-4 rounded-lg  ">
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-row justify-between  m-4  ">
-          <h1 className="sm:text-xl text-lg font-semibold">Trips</h1>
-          <button className="flex flex-row gap-2 border-0 decoration-none bg-transparent shadow-xs">
-            <p className="sm:block hidden">Edit</p>
-            <CiEdit color="orange" size={22} />
-          </button>
+    <>
+      <div className="h-auto overflow-y-auto bg-white w-[95x%] relative m-4 rounded-lg  ">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row justify-between  m-4  ">
+            <h1 className="sm:text-xl text-lg font-semibold">Trips</h1>
+            <button className="flex flex-row gap-2 border-0 decoration-none bg-transparent shadow-xs">
+              <p className="sm:block hidden">Edit</p>
+              <CiEdit color="orange" size={22} />
+            </button>
+          </div>
+          <div className="w-full h-[0.8px] bg-gray-300"></div>
         </div>
-        <div className="w-full h-[0.8px] bg-gray-300"></div>
-      </div>
-      <div className="justify-center items-center w-full  h-auto">
-        <div className="relative overflow-x-auto  justify-center items-center  ">
-          <table className="text-md text-left text-gray-500 m-auto lg:w-full md:w-full h-full">
-            <thead className="text-xs text-gray-700 uppercase ">
-              <tr className="border-b  border-gray-300 ">
-                <td scope="col" className="pl-6 px-4 py-4 text-lg">
-                  Trip Package Name
-                </td>
-                <td scope="col" className="pl-6 px-4 py-4 text-lg">
-                  Total Seats Limit
-                </td>
-                <td scope="col" className="px-4 py-4 text-lg">
-                  Trip Date
-                </td>
-                <td scope="col" className="px-4 py-4 text-lg">
-                  Seats Booked
-                </td>
-                <td scope="col" className="px-4 py-4 text-lg">
-                  Remove
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.map((item: any) => (
-                <tr className="bg-white border-b hover:bg-gray-50">
-                  <td
-                    scope="row"
-                    className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
-                  >
-                    <p className="text-lg py-2 ">
-                      {" "}
-                      {item?.tnp_packages?.package_name}
-                    </p>
+        <div className="justify-center items-center w-full  h-auto">
+          <div className="relative overflow-x-auto  justify-center items-center  ">
+            <table className="text-md text-left text-gray-500 m-auto lg:w-full md:w-full h-full">
+              <thead className="text-xs text-gray-700 uppercase ">
+                <tr className="border-b  border-gray-300 ">
+                  <td scope="col" className="pl-6 px-4 py-4 text-lg">
+                    Trip Package Name
                   </td>
-                  <td
-                    scope="row"
-                    className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
-                  >
-                    <p className="text-lg py-2 ">
-                      {" "}
-                      {item?.tnp_packages?.package_total_persons}
-                    </p>
+                  <td scope="col" className="pl-6 px-4 py-4 text-lg">
+                    Total Seats Limit
                   </td>
-
-                  <td className="  pl-4 md:pr-0 pr-4 text-lg">
-                    {item?.trip_date}
+                  <td scope="col" className="px-4 py-4 text-lg">
+                    Trip Date
                   </td>
-                  <td className="  pl-4 md:pr-0 pr-4 text-lg">
-                    {item?.trip_booked_count}
+                  <td scope="col" className="px-4 py-4 text-lg">
+                    Seats Booked
                   </td>
-
-                  <td className="  pl-4 py-2 md:pr-0 pr-4">
-                    <button onClick={() => DeleteTrip(item.trip_id)}>
-                      <ImCancelCircle className="text-red-700 hover:text-red-500 text-2xl" />
-                    </button>
+                  <td scope="col" className="px-4 py-4 text-lg">
+                    Remove
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <Pagination
-            current={currentPage}
-            onChange={handlePageChange}
-            pageSize={pageSize} // Number of items per page
-            total={totalItems} // Total number of items
-            showSizeChanger={false} // Hide the size changer
-          />
+              </thead>
+              <tbody>
+                {data?.map((item: any) => (
+                  <tr className="bg-white border-b hover:bg-gray-50">
+                    <td
+                      scope="row"
+                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                    >
+                      <p className="text-lg py-2 ">
+                        {" "}
+                        {item?.tnp_packages?.package_name}
+                      </p>
+                    </td>
+                    <td
+                      scope="row"
+                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                    >
+                      <p className="text-lg py-2 ">
+                        {" "}
+                        {item?.tnp_packages?.package_total_persons}
+                      </p>
+                    </td>
+
+                    <td className="  pl-4 md:pr-0 pr-4 text-lg">
+                      {item?.trip_date}
+                    </td>
+                    <td className="  pl-4 md:pr-0 pr-4 text-lg">
+                      {item?.trip_booked_count}
+                    </td>
+
+                    <td className="  pl-4 py-2 md:pr-0 pr-4">
+                      <button onClick={() => DeleteTrip(item.trip_id)}>
+                        <ImCancelCircle className="text-red-700 hover:text-red-500 text-2xl" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <Pagination
+              current={currentPage}
+              onChange={handlePageChange}
+              pageSize={pageSize} // Number of items per page
+              total={totalItems} // Total number of items
+              showSizeChanger={false} // Hide the size changer
+            />
+          </div>
         </div>
       </div>
-    </div>
+      {loading && <Loader message="Fetching Data" />}
+      {deleting && <Loader message="Deleting Data" />}
+    </>
   );
 };
 export default UserPage;
--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -3,8 +3,11 @@ import { Pagination } from "antd";
 import axios from "axios";
 import { ImCancelCircle } from "react-icons/im";
 import { Toast } from "../../Components/SideToast";
+import Loader from "../../Components/loader";
 const UserPage: React.FC = () => {
   const [currentPage, setCurrPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [deleting, setDeleting] = useState(false);
 
   const handlePageChange = (page: number) => {
     setCurrPage(page);
@@ -13,15 +16,19 @@ const UserPage: React.FC = () => {
   const pageSize = 10;
   const totalItems = data?.length;
   const fetchData = async () => {
+    setLoading(true);
+
     let res = await axios.get(
-      `${process.env.REACT_APP_SERVER_URL}/getUsers?page=${currentPage}`
+      `${process.env.REACT_APP_SERVER_URL}/getUsers?page=${currentPage}&limit=${pageSize}`
     );
     setData(res.data.data);
+    setLoading(false);
   };
   useEffect(() => {
     fetchData();
   }, []);
   const DeleteUser = async (id: number) => {
+    setDeleting(true);
     await axios
       .delete(`${process.env.REACT_APP_SERVER_URL}/getUsers?id=${id}`)
       .then((res) => {
@@ -36,61 +43,65 @@ const UserPage: React.FC = () => {
           title: err.response.data,
         });
       });
+    setDeleting(false);
 
     fetchData();
   };
   return (
-    <div className="h-auto overflow-y-auto bg-white w-[95x%] relative m-4 rounded-lg  ">
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-row justify-between  m-4  ">
-          <h1 className="sm:text-xl text-lg font-semibold">Users</h1>
+    <>
+      <div className="h-auto overflow-y-auto bg-white w-[95x%] relative m-4 rounded-lg  ">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row justify-between  m-4  ">
+            <h1 className="sm:text-xl text-lg font-semibold">Users</h1>
+          </div>
+          <div className="w-full h-[0.8px] bg-gray-300"></div>
         </div>
-        <div className="w-full h-[0.8px] bg-gray-300"></div>
-      </div>
-      <div className="justify-center items-center w-full  h-auto">
-        <div className="relative overflow-x-auto  justify-center items-center  ">
-          <table className="text-md text-left text-gray-500 m-auto lg:w-full md:w-full h-full">
-            <thead className="text-xs text-gray-700 uppercase ">
-              <tr className="border-b  border-gray-300 ">
-                <td scope="col" className="pl-6 px-4 py-4 text-lg">
-                  First Name
-                </td>
-                <td scope="col" className="pl-6 px-4 py-4 text-lg">
-                  Last Name
-                </td>
-                <td scope="col" className="px-4 py-4 text-lg">
-                  Email
-                </td>
-                <td scope="col" className="px-4 py-4 text-lg">
-                  Login Count
-                </td>
-                <td scope="col" className="px-4 py-4 text-lg">
-                  Remove
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.map((item: any) => (
-                <tr className="bg-white border-b hover:bg-gray-50">
-                  <td
-                    scope="row"
-                    className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
-                  >
-                    <p className="text-lg py-2 "> {item?.name}</p>
+        <div className="justify-center items-center w-full  h-auto">
+          <div className="relative overflow-x-auto  justify-center items-center  ">
+            <table className="text-md text-left text-gray-500 m-auto lg:w-full md:w-full h-full">
+              <thead className="text-xs text-gray-700 uppercase ">
+                <tr className="border-b  border-gray-300 ">
+                  <td scope="col" className="pl-6 px-4 py-4 text-lg">
+                    First Name
                   </td>
-                  <td
-                    scope="row"
-                    className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
-                  >
-                    <p className="text-lg py-2 "> {item?.lname}</p>
+                  <td scope="col" className="pl-6 px-4 py-4 text-lg">
+                    Last Name
                   </td>
+                  <td scope="col" className="px-4 py-4 text-lg">
+                    Email
+                  </td>
+                  <td scope="col" className="px-4 py-4 text-lg">
+                    Login Count
+                  </td>
+                  <td scope="col" className="px-4 py-4 text-lg">
+                    Remove
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.map((item: any) => (
+                  <tr className="bg-white border-b hover:bg-gray-50">
+                    <td
+                      scope="row"
+                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                    >
+                      <p className="text-lg py-2 "> {item?.name}</p>
+                    </td>
+                    <td
+                      scope="row"
+                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                    >
+                      <p className="text-lg py-2 "> {item?.lname}</p>
+                    </td>
 
-                  <td className="  pl-4 md:pr-0 pr-4 text-lg">{item.email}</td>
-                  <td className="  pl-4 md:pr-0 pr-4 text-lg">
-                    {item.login_count}
-                  </td>
+                    <td className="  pl-4 md:pr-0 pr-4 text-lg">
+                      {item.email}
+                    </td>
+                    <td className="  pl-4 md:pr-0 pr-4 text-lg">
+                      {item.login_count}
+                    </td>
 
-                  {/* <td
+                    {/* <td
                     className={` pl-4  py-2 md:pr-0 pr-4 text-lg ${
                       item.status == "active"
                         ? "text-green-500"
@@ -100,15 +111,15 @@ const UserPage: React.FC = () => {
                     {item?.status == "active" ? "Active" : "Not Active"}
                   </td> */}
 
-                  <td className="  pl-4 py-2 md:pr-0 pr-4">
-                    <button onClick={() => DeleteUser(item.id)}>
-                      <ImCancelCircle className="text-red-700 hover:text-red-500 text-2xl" />
-                    </button>
-                  </td>
-                </tr>
-              ))}
+                    <td className="  pl-4 py-2 md:pr-0 pr-4">
+                      <button onClick={() => DeleteUser(item.id)}>
+                        <ImCancelCircle className="text-red-700 hover:text-red-500 text-2xl" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
 
-              {/* <tr colSpan={7} className="">
+                {/* <tr colSpan={7} className="">
                   <td colSpan={7} className="flex flex-row gap-2">
                     {currentPage > 1 && (
                       <GrLinkPrevious
@@ -130,18 +141,21 @@ const UserPage: React.FC = () => {
                     )}
                   </td>
                 </tr> */}
-            </tbody>
-          </table>
-          <Pagination
-            current={currentPage}
-            onChange={handlePageChange}
-            total={totalItems} // Total number of items
-            pageSize={pageSize} // Number of items per page
-            showSizeChanger={false} // Hide the size changer
-          />
+              </tbody>
+            </table>
+            <Pagination
+              current={currentPage}
+              onChange={handlePageChange}
+              total={totalItems} // Total number of items
+              pageSize={pageSize} // Number of items per page
+              showSizeChanger={false} // Hide the size changer
+            />
+          </div>
         </div>
       </div>
-    </div>
+      {loading && <Loader message="Fetching Data" />}
+      {deleting && <Loader message="Deleting Data" />}
+    </>
   );
 };
 export default UserPage;
