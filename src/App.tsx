4918c9b7aafcca7bea8923c58ade9bd11c5fe195
usman-90 <siddiquiusman328@gmail.com>
2024-05-14 23:13:57 +0500

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -14,39 +14,41 @@ import Analytics from "./Pages/Analytics/Analytics";
 import Banners from "./Pages/Banners/banners";
 import Testimonials from "./Pages/Testimonials/Testimonials";
 import CarBookings from "./Pages/CarBookings";
+import Login from "./Pages/Login";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity,
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity,
+        },
     },
-  },
 });
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="" element={<Dashboard />} />
-            <Route path="users" element={<UserPage />} />
-            <Route path="departure" element={<Departure />} />
-            <Route path="packages" element={<TourPackages />} />
-            <Route path="trips" element={<Trips />} />
-            <Route path="banners" element={<Banners />} />
-            <Route path="carRental" element={<CarRental />} />
-            <Route path="carbookings" element={<CarBookings />} />
-            <Route path="hotel" element={<Hotel />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="analytics" element={<Analytics />} />
-            <Route path="testimonials" element={<Testimonials />} />
-          </Route>
-        </Routes>
-      </QueryClientProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <Routes>
+                    <Route path="login" element={<Login />} />
+                    <Route path="/" element={<Layout />}>
+                        <Route path="" element={<Dashboard />} />
+                        <Route path="users" element={<UserPage />} />
+                        <Route path="departure" element={<Departure />} />
+                        <Route path="packages" element={<TourPackages />} />
+                        <Route path="trips" element={<Trips />} />
+                        <Route path="banners" element={<Banners />} />
+                        <Route path="carRental" element={<CarRental />} />
+                        <Route path="carbookings" element={<CarBookings />} />
+                        <Route path="hotel" element={<Hotel />} />
+                        <Route path="settings" element={<Settings />} />
+                        <Route path="analytics" element={<Analytics />} />
+                        <Route path="testimonials" element={<Testimonials />} />
+                    </Route>
+                </Routes>
+            </QueryClientProvider>
+        </BrowserRouter>
+    );
 };
 
 export default App;
