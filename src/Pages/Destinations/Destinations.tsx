62e276259c42900053a33884868bcee7132b29c0
salman <alysalman15@gmail.com>
2024-05-24 11:08:46 +0500

    add confirmation delete modal for destination screen

--- src/Pages/Destinations/Destinations.tsx
+++ src/Pages/Destinations/Destinations.tsx
@@ -7,6 +7,7 @@ import Loader from "../../Components/loader";
 import { Button } from "antd";
 import { RiAddLine } from "react-icons/ri";
 import DestinationInsertionBox from "../../Components/DestinationsComp/DestinationInsertionBox";
+import DeleteModal from "./deleteModal";
 
 const Destinations: React.FC = () => {
   const [openBox, setOpenBox] = useState(false);
@@ -14,12 +15,16 @@ const Destinations: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [deleting, setDeleting] = useState(false);
   const [data, setData] = useState([]);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState<any>(null);
 
   const handlePageChange = (page: number) => {
     setCurrPage(page);
   };
+
   const pageSize = 10;
   const totalItems = data?.length;
+
   const fetchData = async () => {
     setLoading(true);
     let res = await axios.get(
@@ -28,10 +33,16 @@ const Destinations: React.FC = () => {
     setData(res.data.data);
     setLoading(false);
   };
-  const DeleteDestination = async (id: number) => {
+
+  const confirmDelete = (item: any) => {
+    setItemToDelete(item);
+    setIsDeleteModalOpen(true);
+  };
+
+  const deleteDestination = async (item: any) => {
     setDeleting(true);
     await axios
-      .delete(`${process.env.REACT_APP_SERVER_URL}/admin/getDestinations?id=${id}`)
+      .delete(`${process.env.REACT_APP_SERVER_URL}/admin/getDestinations?id=${item.destination_id}`)
       .then((_) => {
         Toast.fire({
           icon: "success",
@@ -45,19 +56,19 @@ const Destinations: React.FC = () => {
         });
       });
     setDeleting(false);
-
+    setIsDeleteModalOpen(false);
     fetchData();
   };
+
   useEffect(() => {
     fetchData();
   }, []);
-  console.log("destination here",data);
 
   return (
     <>
-      <div className="h-auto overflow-y-auto bg-white w-[95x%] relative m-4 rounded-lg  ">
+      <div className="h-auto overflow-y-auto bg-white w-[95%] relative m-4 rounded-lg">
         <div className="flex flex-col gap-4">
-          <div className="flex flex-row justify-between  m-4  ">
+          <div className="flex flex-row justify-between  m-4">
             <h1 className="sm:text-xl text-lg font-semibold">Destinations</h1>
 
             <Button
@@ -75,11 +86,11 @@ const Destinations: React.FC = () => {
           </div>
           <div className="w-full h-[0.8px] bg-gray-300"></div>
         </div>
-        <div className="justify-center items-center w-full  h-auto">
-          <div className="relative overflow-x-auto  justify-center items-center  ">
+        <div className="justify-center items-center w-full h-auto">
+          <div className="relative overflow-x-auto justify-center items-center">
             <table className="text-md text-left text-gray-500 m-auto lg:w-full md:w-full h-full">
-              <thead className="text-xs text-gray-700 uppercase ">
-                <tr className="border-b  border-gray-300 ">
+              <thead className="text-xs text-gray-700 uppercase">
+                <tr className="border-b border-gray-300">
                   <td scope="col" className="pl-6 px-4 py-4 text-lg">
                     Destination Category
                   </td>
@@ -99,46 +110,42 @@ const Destinations: React.FC = () => {
               </thead>
               <tbody>
                 {data?.map((item: any) => (
-                  <tr className="bg-white border-b hover:bg-gray-50">
+                  <tr className="bg-white border-b hover:bg-gray-50" key={item.destination_id}>
                     <td
                       scope="row"
-                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                      className="text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg"
                     >
-                      <p className="text-lg py-2 ">
-                        {" "}
+                      <p className="text-lg py-2">
                         {item?.destination_category_name}
                       </p>
                     </td>
                     <td
                       scope="row"
-                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                      className="text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg"
                     >
-                      <p className="text-lg py-2 ">
-                        {" "}
+                      <p className="text-lg py-2">
                         {item?.destination_region_name}
                       </p>
                     </td>
                     <td
                       scope="row"
-                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                      className="text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg"
                     >
-                      <p className="text-lg py-2 ">
-                        {" "}
+                      <p className="text-lg py-2">
                         {item?.destination_name}
                       </p>
                     </td>
                     <td
                       scope="row"
-                      className=" text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg "
+                      className="text-gray-900 whitespace-nowrap pl-6 py-2 md:pr-0 pr-4 text-lg"
                     >
-                      <p className="text-lg py-2 ">
-                        {" "}
+                      <p className="text-lg py-2">
                         {item?.destination_minimum_tour_days}
                       </p>
                     </td>
-                    
-                    <td className="  pl-4 py-2 md:pr-0 pr-4">
-                      <button onClick={() => DeleteDestination(item.destination_id)}>
+
+                    <td className="pl-4 py-2 md:pr-0 pr-4">
+                      <button onClick={() => confirmDelete(item)}>
                         <ImCancelCircle className="text-red-700 hover:text-red-500 text-2xl" />
                       </button>
                     </td>
@@ -150,16 +157,23 @@ const Destinations: React.FC = () => {
             <Pagination
               current={currentPage}
               onChange={handlePageChange}
-              pageSize={pageSize} // Number of items per page
-              total={totalItems} // Total number of items
-              showSizeChanger={false} // Hide the size changer
+              pageSize={pageSize}
+              total={totalItems}
+              showSizeChanger={false}
             />
           </div>
         </div>
       </div>
       {loading && <Loader message="Fetching Data" />}
       {deleting && <Loader message="Deleting Data" />}
+      <DeleteModal
+        isDeleteModalOpen={isDeleteModalOpen}
+        setIsDeleteModalOpen={setIsDeleteModalOpen}
+        onDeleteHandle={deleteDestination}
+        item={itemToDelete}
+      />
     </>
   );
 };
+
 export default Destinations;
---
+++ src/Pages/Destinations/deleteModal.tsx
@@ -0,0 +1,35 @@
+import { Modal } from "antd";
+import React from "react";
+
+interface Props {
+  isDeleteModalOpen: boolean;
+  setIsDeleteModalOpen: (isDeleteModalOpen: boolean) => void;
+  onDeleteHandle: (item: any) => void;
+  item: any;
+}
+
+const DeleteModal: React.FC<Props> = ({
+  isDeleteModalOpen,
+  onDeleteHandle,
+  setIsDeleteModalOpen,
+  item,
+}) => {
+  return (
+    <div>
+      <Modal
+        title="Delete Destination"
+        centered
+        open={isDeleteModalOpen}
+        onOk={() => onDeleteHandle(item)}
+        onCancel={() => setIsDeleteModalOpen(false)}
+        width={600}
+      >
+        <p className="text-red-500">
+          Are you sure you want to delete this destination?
+        </p>
+      </Modal>
+    </div>
+  );
+};
+
+export default DeleteModal;
